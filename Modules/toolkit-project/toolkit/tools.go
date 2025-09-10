0c4e153caa0b519e4dd46216c5d5dee9f5ea3afc
Christo Lombard <christo.lombard@live.co.za>
2025-09-10 21:55:33 +0200

    Continued with toolkit project.

--- Modules/toolkit-project/toolkit/tools.go
+++ Modules/toolkit-project/toolkit/tools.go
@@ -1,11 +1,22 @@
 package toolkit
 
-import "crypto/rand"
+import (
+	"crypto/rand"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+)
 
 const randomStringSource = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789_"
 
 // Tools is the type to instantiate this module
 type Tools struct {
+	MaxFileSize      int
+	AllowedFileTypes []string
 }
 
 // Generate a random string of length n
@@ -19,4 +30,132 @@ func (t *Tools) RandomString(n int) string {
 	}
 
 	return string(s)
+}
+
+type UploadedFile struct {
+	NewFileName      string
+	OriginalFileName string
+	FileSize         int64
+	//TODO play with hash code if they re-upload the same file again but there might be changes just to replace it?
+}
+
+func (t *Tools) UploadSingleFile(r *http.Request, uploadDir string, rename ...bool) (*UploadedFile, error) {
+	renameFile := true
+
+	if len(rename) > 0 {
+		renameFile = rename[0]
+	}
+
+	files, err := t.UploadFiles(r, uploadDir, renameFile)
+	if err != nil {
+		return nil, err
+
+	}
+
+	return files[0], nil
+}
+
+func (t *Tools) UploadFiles(r *http.Request, uploadDir string, rename ...bool) ([]*UploadedFile, error) {
+	renameFile := true
+
+	if len(rename) > 0 {
+		renameFile = rename[0]
+	}
+
+	var uploadedFiles []*UploadedFile
+
+	if t.MaxFileSize == 0 {
+		t.MaxFileSize = 1024 * 1024 * 1024
+	}
+
+	err := r.ParseMultipartForm(int64(t.MaxFileSize))
+
+	if err != nil {
+		return nil, errors.New("the upload file is too big")
+	}
+
+	for _, fHeaders := range r.MultipartForm.File {
+
+		for _, hdr := range fHeaders {
+
+			uploadedFiles, err = func(uploadFiles []*UploadedFile) ([]*UploadedFile, error) {
+
+				var uploadedFile UploadedFile
+				infile, err := hdr.Open()
+
+				if err != nil {
+					return nil, err
+				}
+
+				defer infile.Close()
+
+				//TODO: Move everything regarding the management of the file to a private struct with methods
+				// this struct will have a interface and some methods that can be called to get information of the file once.
+				// Maybe see if the result pattern can work for this for the errors being returned or not.
+
+				//Look at the first 512 bytes of the file to check what the file type is.
+				buff := make([]byte, 512)
+
+				_, err = infile.Read(buff)
+
+				if err != nil {
+					return nil, err
+				}
+
+				allowed := false
+				fileType := http.DetectContentType(buff)
+
+				if len(t.AllowedFileTypes) > 0 {
+					for _, x := range t.AllowedFileTypes {
+						if strings.EqualFold(fileType, x) {
+							allowed = true
+						}
+					}
+				} else {
+					allowed = true
+				}
+
+				if !allowed {
+					return nil, errors.New("uploaded file type not permitted")
+				}
+
+				_, err = infile.Seek(0, 0)
+
+				if err != nil {
+					return nil, err
+				}
+
+				if renameFile {
+					uploadedFile.NewFileName = fmt.Sprintf("%s%s", t.RandomString(25), filepath.Ext(hdr.Filename))
+				} else {
+					uploadedFile.NewFileName = hdr.Filename
+				}
+
+				uploadedFile.OriginalFileName = hdr.Filename
+				var outfile *os.File
+				defer outfile.Close()
+
+				if outfile, err = os.Create(filepath.Join(uploadDir, uploadedFile.NewFileName)); err != nil {
+					return nil, err
+				} else {
+
+					fileSize, err := io.Copy(outfile, infile)
+					if err != nil {
+						return nil, err
+					}
+
+					uploadedFile.FileSize = fileSize
+				}
+
+				uploadedFiles = append(uploadedFiles, &uploadedFile)
+
+				return uploadedFiles, nil
+
+			}(uploadedFiles)
+
+		}
+
+	}
+
+	return uploadedFiles, nil
 }
